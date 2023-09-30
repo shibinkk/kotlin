92943c829d7d151106932561287405f2a57b33ff
= <shibinkk17@gmail.com>
2023-09-30 16:08:46 +0530

    sort using loop

--- sort.kt
+++ sort.kt
@@ -1,5 +1,14 @@
 fun main(){
-    val list = listOf(6,2,9,5,8,1,3)
-    val sort = list.sorted()
-    println(sort)
+    val list = mutableListOf(6,2,9,5,8,1,3)
+    val n =list.size
+    for (i in 0 until n-1) {
+        for (j in 0 until n-i-1) {
+            if(list[j] > list[j+1]){
+            val temp = list[j]
+            list[j] = list[j+1]
+            list[j+1] = temp
+            }
+        }
+    }
+     println("${list.joinToString()}")
 }
